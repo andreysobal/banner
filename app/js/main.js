de7eadce93a5412ed2de21805142064dbb1a6415
andreysobal <say6287@gmail.com>
2017-08-28 21:54:26 +0300

    Changed method of font-minimization

--- app/js/main.js
+++ app/js/main.js
@@ -1,106 +1,112 @@
-const strings = [
-  {
-    lang: "es",
-    textTop: "Mantén Seguras Tus Fotos y Vídeos Privados",
-    textBottom: "Prueba gratuita de 7 días"
-  },
-  {
-    lang: "de",
-    textTop: "Halten Sie Ihre privaten Fotos & Videos geheim",
-    textBottom: "7-tägige Gratis-Version"
-  },
-  {
-    lang: "fr",
-    textTop: "Protégez Vos Photos et Vidéos Privées",
-    textBottom: "7 jours d'essai gratuit"
-  },
-  {
-    lang: "ja",
-    textTop: "プライベート写真と 動画を安全に 保つ",
-    textBottom: "7日間無料トライアル"
-  },
-  {
-    lang: "it",
-    textTop: "Tieni al Sicuro i Tuoi Video e Foto Private",
-    textBottom: "Prova gratuita di 7 giorni"
-  },
-  {
-    lang: "ru",
-    textTop: "Держите Личные Фото и Видео под Надежной Защитой",
-    textBottom: "7-дневная пробная версия"
-  }
-]
+window.onload = function() {
+  const strings = [
+    {
+      lang: "es",
+      textTop: "Mantén Seguras Tus Fotos y Vídeos Privados",
+      textBottom: "Prueba gratuita de 7 días"
+    },
+    {
+      lang: "de",
+      textTop: "Halten Sie Ihre privaten Fotos & Videos geheim",
+      textBottom: "7-tägige Gratis-Version"
+    },
+    {
+      lang: "fr",
+      textTop: "Protégez Vos Photos et Vidéos Privées",
+      textBottom: "7 jours d'essai gratuit"
+    },
+    {
+      lang: "ja",
+      textTop: "プライベート写真と 動画を安全に 保つ",
+      textBottom: "7日間無料トライアル"
+    },
+    {
+      lang: "it",
+      textTop: "Tieni al Sicuro i Tuoi Video e Foto Private",
+      textBottom: "Prova gratuita di 7 giorni"
+    },
+    {
+      lang: "ru",
+      textTop: "Держите Личные Фото и Видео под Надежной Защитой",
+      textBottom: "7-дневная пробная версия"
+    }
+  ]
 
-/*Receive lang from server*/
-/*fetch('./data.db')
-	.then(response => response.json())
-  .then(data => {
-  	if (data.lang) {
-      let lang = data.lang;
-      document.getElementsByTagName('html')[0].setAttribute("lang", lang);
-      let text;
-      strings.forEach((item, i) => {
-        if (item.lang === lang){
-          text = item;
-        }//end if
-      })//end forEach
+  /*Receive lang from server*/
+  //uncorrect method of language selection
+  /*fetch('./data.db')
+  	.then(response => response.json())
+    .then(data => {
+    	if (data.lang) {
+        let lang = data.lang;
+        document.getElementsByTagName('html')[0].setAttribute("lang", lang);
+        let text;
+        strings.forEach((item, i) => {
+          if (item.lang === lang){
+            text = item;
+          }//end if
+        })//end forEach
 
-      const height = parseInt(getComputedStyle(document.getElementById('content')).height);
-     
-      let textTop = document.createElement("div");
-      textTop.setAttribute("id", "text_top");
-      textTop.innerHTML = text.textTop;
+        const height = parseInt(getComputedStyle(document.getElementById('content')).height);
+       
+        let textTop = document.createElement("div");
+        textTop.setAttribute("id", "text_top");
+        textTop.innerHTML = text.textTop;
 
-      let textBottom = document.createElement("div");
-      textBottom.setAttribute("id", "text_bottom");
-      textBottom.innerHTML = text.textBottom;
-     
-      document.getElementById('text_top').remove();
-      document.getElementById('text_bottom').remove();
-      let content = document.getElementById('content');
-      content.appendChild(textTop);
-      content.appendChild(textBottom);
+        let textBottom = document.createElement("div");
+        textBottom.setAttribute("id", "text_bottom");
+        textBottom.innerHTML = text.textBottom;
+       
+        document.getElementById('text_top').remove();
+        document.getElementById('text_bottom').remove();
+        let content = document.getElementById('content');
+        content.appendChild(textTop);
+        content.appendChild(textBottom);
 
-      let $fontSize = parseInt(getComputedStyle(content).fontSize);
-      resizeFont (content, $fontSize, height);
-    }//end if
-  })
-  .catch (error => {
-  	alert(error);
-  });*/
+        let $fontSize = parseInt(getComputedStyle(content).fontSize);
+        resizeFont (content, $fontSize, height);
+      }//end if
+    })
+    .catch (error => {
+    	alert(error);
+    });*/
 
-var url = window.location.search;
-console.log("Status text: ", url);
-if (url){
-  let lang = url.replace(/^.*?\=/,"");
-  console.log("lang", lang);
-  strings.forEach (function(item, i){
-    if (item.lang == lang){
-      let text = item;
-      console.log("lang: ", lang, ", text: ", text.textTop);
-      document.getElementsByTagName('html')[0].setAttribute("lang", lang);
+  //correct new method of language selection
+  var url = window.location.search;
+  if (url){
+    let lang = url.replace(/^.*?\=/,"");
+    strings.forEach (function(item, i){
+      if (item.lang == lang){
+        let text = item;
+        document.getElementsByTagName('html')[0].setAttribute("lang", lang);
 
-      const height = parseInt(getComputedStyle(document.getElementById('content')).height);
-      let textTop = document.createElement("div");
-      textTop.setAttribute("id", "text_top");
-      textTop.innerHTML = text.textTop;
-      let textBottom = document.createElement("div");
-      textBottom.setAttribute("id", "text_bottom");
-      textBottom.innerHTML = text.textBottom;
-      document.getElementById('text_top').remove();
-      document.getElementById('text_bottom').remove();
-      let content = document.getElementById('content');
-      content.appendChild(textTop);
-      content.appendChild(textBottom);
-      let $fontSize = parseInt(getComputedStyle(content).fontSize);
-      resizeFont (content, $fontSize, height);
-    }//end if
-  })//end forEach
-}//end if
+        const topHeight = parseInt(getComputedStyle(document.getElementById('text_top')).height);
+        const bottomHeight = parseInt(getComputedStyle(document.getElementById('text_bottom')).height);
 
+        let textTop = document.createElement("div");
+        textTop.setAttribute("id", "text_top");
+        textTop.innerHTML = text.textTop;
+        let textBottom = document.createElement("div");
+        textBottom.setAttribute("id", "text_bottom");
+        textBottom.innerHTML = text.textBottom;
+        document.getElementById('text_top').remove();
+        document.getElementById('text_bottom').remove();
+        let content = document.getElementById('content');
+        content.appendChild(textTop);
+        content.appendChild(textBottom);
+
+        let $fontSize = parseInt(getComputedStyle(document.getElementById('text_top')).fontSize);
+        resizeFont (document.getElementById('text_top'), $fontSize, topHeight);
+        $fontSize = parseInt(getComputedStyle(document.getElementById('text_bottom')).fontSize);
+        resizeFont (document.getElementById('text_bottom'), $fontSize, bottomHeight);
+      }//end if
+    })//end forEach
+  }//end if
+  return false;
+}//end onload
   /*for gradually fontSize reducing*/
   function resizeFont (content, $fontSize, height){
-    $fontSize = ($fontSize)*0.9;
+    $fontSize = ($fontSize)*0.99;
     content.style.fontSize = $fontSize+"em";
     let newHeight = parseInt(getComputedStyle(content).height);
     if (newHeight > height) {
